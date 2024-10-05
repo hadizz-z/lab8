7dc267ef92233a12d57dcb7607b4cd9bf1b6d500
Хадижат Исакова <seastar30t@gmail.com>
2024-10-05 08:02:52 +0300

    Добавлен файл time.pas

---
+++ time.pas
@@ -0,0 +1,22 @@
+﻿begin
+  var n := ReadInteger('Введите год');
+  Assert(n > 0);
+  if n mod 4 = 0 then
+  begin
+    if (n mod 100 = 0) and (n mod 400 <> 0) then
+      Writeln('Год не високосный')
+    else
+      Writeln('Год високосный');
+  end
+  else
+    Writeln('Год не високосный');
+end.
+
+{
+Введите год 2024
+Год високосный
+Введите год 1999
+Год не високосный
+Введите год 1
+Год не високосный
+}
